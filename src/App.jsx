7d98a4af9d9b0a62459ec8000588baafc063c5da
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-03-04 15:12:58 +0200

    chore : added data

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,4 @@
-import ScreenSpliter from "./layout/ScreenSpliter";
-const LeftComponent = ({ color }) => {
-  return (
-    <div
-      style={{
-        backgroundColor: color,
-      }}
-    >
-      Left
-    </div>
-  );
-};
-const RightComponent = ({ color }) => {
-  return (
-    <div
-      style={{
-        backgroundColor: color,
-      }}
-    >
-      Right
-    </div>
-  );
-};
-
 function App() {
-  return (
-    <div className="flex h-screen">
-      <ScreenSpliter
-        leftWidth={1}
-        rightWidth={3}
-        leftBackgroundColor="#6767"
-        rightBackgroundColor="#123678"
-      >
-        <LeftComponent color={"red"} />
-        <RightComponent color={"yellow"} />
-      </ScreenSpliter>
-    </div>
-  );
+  return <div className="flex h-screen"></div>;
 }
 export default App;
