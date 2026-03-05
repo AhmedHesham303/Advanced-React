fc4d43bb4792d0dbd6b5fffe41c7310491bab4db
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-03-05 14:29:40 +0200

    chore : server setup

--- codes/server.js
+++ codes/server.js
@@ -1,10 +1,10 @@
-import express from "express";
+const express = require("express");
+
 const app = express();
 
 app.use(express.json());
 
 let currentUser = {
-  id: "1",
   name: "Sarah Waters",
   age: 55,
   country: "United Kingdom",
@@ -13,21 +13,18 @@ let currentUser = {
 
 let users = [
   {
-    id: "1",
     name: "Sarah Waters",
     age: 55,
     country: "United Kingdom",
     books: ["Fingersmith", "The Night Watch"],
   },
   {
-    id: "2",
     name: "Haruki Murakami",
     age: 71,
     country: "Japan",
     books: ["Norwegian Wood", "Kafka on the Shore"],
   },
   {
-    id: "3",
     name: "Chimamanda Ngozi Adichie",
     age: 43,
     country: "Nigeria",
@@ -37,21 +34,18 @@ let users = [
 
 let books = [
   {
-    id: "1",
     name: "To Kill a Mockingbird",
     pages: 281,
     title: "Harper Lee",
     price: 12.99,
   },
   {
-    id: "2",
     name: "The Catcher in the Rye",
     pages: 224,
     title: "J.D. Salinger",
     price: 9.99,
   },
   {
-    id: "3",
     name: "The Little Prince",
     pages: 85,
     title: "Antoine de Saint-Exupéry",
@@ -63,6 +57,7 @@ app.get("/current-user", (req, res) => res.json(currentUser));
 
 app.get("/users/:id", (req, res) => {
   const { id } = req.params;
+  console.log(id);
   res.json(users.find((user) => user.id === id));
 });
 
@@ -86,5 +81,5 @@ app.get("/books/:id", (req, res) => {
 
 let SERVER_PORT = 9090;
 app.listen(SERVER_PORT, () =>
-  console.log(`Server is listening on port: ${SERVER_PORT}`),
+  console.log(`Server is listening on port: ${SERVER_PORT}`)
 );
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,4 @@
-import { authors } from "./data/authors";
-import LargeAuthorListItem from "./DesignPatterns/layout/lists/authors/LargeListItem";
-import RegularList from "./DesignPatterns/layout/lists/RegularList";
-import Modal from "./DesignPatterns/layout/Modal";
-
 function App() {
-  return (
-    <Modal>
-      <RegularList
-        sourceName={"author"}
-        items={authors}
-        ItemComponent={LargeAuthorListItem}
-      />
-    </Modal>
-  );
+  return <div></div>;
 }
 export default App;
