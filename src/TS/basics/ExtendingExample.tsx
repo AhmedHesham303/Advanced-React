91fb7fe11300b4b268e4de36c6360fa7db343aab
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-03-15 13:03:18 +0200

    enhanc btn component

--- src/TS/basics/ExtendingExample.tsx
+++ src/TS/basics/ExtendingExample.tsx
@@ -1,6 +1,13 @@
 import type { ComponentPropsWithoutRef } from "react";
 
-type ButtonProps = ComponentPropsWithoutRef<"button">;
-export default function Buttun<ButtonProps>({}) {
-  return <div>ExtendingExample</div>;
+type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  variant?: "primary" | "secondary";
+};
+
+export default function Button({ variant, children, ...props }: ButtonProps) {
+  return (
+    <button {...props} className={`btn-${variant}`}>
+      {children}
+    </button>
+  );
 }
